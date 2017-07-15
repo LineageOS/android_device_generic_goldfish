4a67b1be153178ec47f53cc48ad990c4f7aeaf73
Tim Schumacher <timschumi2@arcor.de>
2017-07-15 20:21:23 +0200

    goldfish: Rebrand to LineageOS
    Keeping the defconfig name unchanged for now
    
    Change-Id: Ib43406065560879900748bcf99745f1aac11e5a2

--- lineage.mk
+++ lineage.mk
@@ -11,7 +11,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 $(call inherit-product, device/generic/goldfish/goldfish.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_goldfish
+PRODUCT_NAME := lineage_goldfish
 PRODUCT_DEVICE := generic
-PRODUCT_BRAND := CyanogenMod
-PRODUCT_MODEL := Full CyanogenMod on Emulator
+PRODUCT_BRAND := LineageOS
+PRODUCT_MODEL := Full LineageOS on Emulator
